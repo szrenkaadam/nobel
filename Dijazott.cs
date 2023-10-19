9f68b901650f76fe582cc6dbde6bae255a2ac951
Ádám Szrenka <szrenka.adam@gmail.com>
2023-10-19 10:23:30 +0200

    Add project files.

---
+++ Dijazott.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nobel
+{
+    class Dijazott
+    {
+        public int Ev { get; set; }
+        public  string Nev { get; set; }
+        public string SzHStr { get; set; }
+
+        public (int Szuletes, int? Halal) SzH { get; set; }
+        public string Orszag { get; set; }
+
+
+        
+        public Dijazott(string a)
+        {
+            var v = a.Split(";");
+
+            Ev = int.Parse(v[0]);
+            Nev = v[1];
+            SzHStr = v[2];
+            var szhv = v[2].Split('-');
+            int? h = szhv[1].Length == 0 ? null : int.Parse(szhv[1]);
+            SzH = (int.Parse(szhv[0]), h);
+
+            Orszag = v[3];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace nobel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dijazottak = new List<Dijazott>();
+            using var sr = new StreamReader(@"..\..\..\src\orvos_nobeldijasok.txt",
+                Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                dijazottak.Add(new(sr.ReadLine()));
+            }
+            Console.WriteLine($"3. feladat: Díjazottak száma: { dijazottak.Count}");
+
+                var f4 = dijazottak.Max(d => d.Ev);
+
+                Console.WriteLine($"4. feladat: Utolsó év: {f4}");
+
+                Console.WriteLine($"5. feladat: Kérem adja meg egy ország kódját:");
+                string ok = Console.ReadLine();
+                var f5 = dijazottak.Where(d => d.Orszag == ok).ToList();
+
+            if (f5.Count == 0) Console.WriteLine("Az országban nem volt díjazott");
+            else if (f5.Count ==1)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+
+
+            }
+        }
+    }
+}
